a1943a63cd11415947d81071df7ceae49747a6f7
mihai <d3monboy009@yahoo.com>
2016-03-14 21:08:57 +0200

    xpath&cssSelectors

--- src/test/java/org/fasttrackit/onlinelibrary/login/FirstLoginTest.java
+++ src/test/java/org/fasttrackit/onlinelibrary/login/FirstLoginTest.java
@@ -1,6 +1,7 @@
 package org.fasttrackit.onlinelibrary.login;
 
 
+import com.sdl.selenium.web.utils.Utils;
 import org.fasttrackit.util.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -42,6 +43,37 @@ public class FirstLoginTest extends TestBase {
         doLogin("eu@fast.com", "");
         assertThaterrorIs("Please enter your password!");
     }
+
+
+    @Test
+    public void succesChangePassword(){
+        openLogInPage();
+        doLogin("eu@fast.com", "eu.pass");
+
+
+        WebElement preferenceButton = driver.findElement(By.xpath("//nav//button"));
+        preferenceButton.click();
+
+        Utils.sleep(300);
+
+        // css selector $$("#preferences-win input[name=password]")
+        WebElement currentPassField = driver.findElement(By.xpath("//div[@id='preferences-win']//input[@name='password']"));
+        currentPassField.sendKeys("eu.pass");
+
+        WebElement newPassField = driver.findElement(By.xpath("//input[@name='newPassword']"));
+        newPassField.sendKeys("eu.pass2");
+
+        WebElement repeatPassField = driver.findElement(By.xpath("//input[@name='newPasswordRepeat']"));
+        repeatPassField.sendKeys("eu.pass2");
+
+        WebElement saveBtn = driver.findElement(By.cssSelector("#preferences-win button.btn-warning"));
+        saveBtn.click();
+
+        WebElement statusElement = driver.findElement(By.cssSelector("#preferences-win .status-msg"));
+        System.out.println(statusElement.getText());
+        assertThat(statusElement.getText(), is("Your password has been successfully changed."));
+    }
+
 
     private void doLogin(String userName, String password) {
         WebElement emailField = driver.findElement(By.id("email"));
